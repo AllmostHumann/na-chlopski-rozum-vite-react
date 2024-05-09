9429516a2b4c9e94d4ec069a04d48c65cd13cc69
AllmostHumann <jakub.gluch92@gmail.com>
2024-05-09 16:56:37 +0200

    tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <div className='w-40 h-40 bg-red-500'>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -28,6 +27,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div className='grid place-items-center'>
+        <div className='w-40 h-40 bg-green-500 grid place-content-center'>CHŁOP</div>
+      </div>
     </>
   )
 }
